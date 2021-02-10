6a48fe7c56b1dc75e5276a7935189bae50b0ecc5
Shanmuga Devi <shanvalar059@gmail.com>
2021-02-10 23:42:25 +0530

    first-commit

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css'
+const mystyle = {
+  color: "black",
+  fontFamily: "Arial"
+};
+const App = () => {
+  return (
+    <div className="App">
+       <body>
+<form>
+        <h1>sample</h1>
+        <p>Title</p>
+        <input
+          type="text"
+        />
+        <p>Description</p>
+        <input
+          type="text"
+        />
+        
+      </form>
+      
+      </body>
+    </div>
+  );
+}
+export default App;
